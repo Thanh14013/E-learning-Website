c63eb17b02b5856c33b5faf537e11f61d0627a17
Thnh8052 <vuthanh250804@gmail.com>
2025-10-16 16:08:43 +0700

    Setup React Router

---
+++ frontend/src/routes/Router.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/layout/Layout.jsx";
+import HomePage from "../pages/homepage/Home.jsx";
+import Login from "../pages/auth/Login.jsx";
+import Register from "../pages/auth/Register.jsx";
+import Courses from "../pages/courses/Courses.jsx";
+import Discussion from "../pages/discussion/Discussion.jsx";
+import NotFound from "../pages/notFound/NotFound.jsx";
+import Dashboard from "../pages/dashboard/Dashboard.jsx";
+import Profile from "../pages/profile/Profile.jsx";
+import ProtectedRoute from "../components/ProtectedRoutes.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "courses", element: <Courses /> },
+      { path: "discussion", element: <Discussion /> },
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { path: "dashboard", element: <Dashboard /> },
+          { path: "profile", element: <Profile /> },
+        ],   
+      },   
+    ],
+  },
+]);
+
+export default router;
