2b170b396689ec6cccf8cf68949eaefcfa584df4
Thnh8052 <vuthanh250804@gmail.com>
2025-10-16 15:36:38 +0700

    Add Homepage+Update global files
    Add Homepage, make changes to App.jsx, App.css, global.css, index.css, main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Router.jsx';
+import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
+import './global.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
