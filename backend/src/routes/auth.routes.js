18399c41f97528b5d23d785f7acb4a3dea1ee3d5
Nguyen Vu Thanh <thanh14704@gmail.com>
2025-10-30 16:46:46 +0700

    feat: update route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,14 @@
-import express from 'express';
-import { register, verifyEmail, refreshAccessToken } from '../controllers/auth.controller.js';
+import express from "express";
+import {
+  register,
+  verifyEmail,
+  refreshAccessToken,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/verify-email/:token', verifyEmail);
-router.post('/refresh-token', refreshAccessToken);
+router.post("/register", register);
+router.post("/verify-email/:token", verifyEmail);
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
