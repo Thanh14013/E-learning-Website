aabc32159540915cb2e5fbcfd8e13da2c8d971e4
PDTnhah <phamduythanh43@gmail.com>
2025-12-12 09:18:32 +0700

    feat: add quiz model

---
+++ backend/src/models/quiz.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const quizSchema = new mongoose.Schema(
+    {
+        courseId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
+            required: true,
+            index: true,
+        },
+
+        lessonId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Lesson",
+            required: true,
+            index: true,
+        },
+
+        title: {
+            type: String,
+            required: [true, "Please provide quiz title."],
+            trim: true,
+        },
+
+        duration: {
+            type: Number, // minutes
+            default: 0,
+        },
+
+        passingScore: {
+            type: Number,
+            required: true,
+        },
+
+        attemptsAllowed: {
+            type: Number,
+            default: 1,
+        },
+
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Compound index to quickly find quiz by lesson + course
+quizSchema.index({ courseId: 1, lessonId: 1 });
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+export default Quiz;
